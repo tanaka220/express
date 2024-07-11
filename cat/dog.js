3eea82801909574abf69960706521d32f65413c1
tanaka220 <2201345013gf@cyber-u.ac.jp>
2024-07-11 16:54:46 +0900

    add dog api

--- cat/dog.js
+++ cat/dog.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 router.get('/', async (req, res) => {
-request('https://dog.ceo/api/breeds/image/random', function (error, response,
+request('https://dog.ceo/dog-api/', function (error, response,
 body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
