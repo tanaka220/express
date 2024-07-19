89e92b72f46b8ca49749ef255a20de6741be12db
tanaka220 <2201345013gf@cyber-u.ac.jp>
2024-07-19 22:55:09 +0900

    add env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -24,7 +24,7 @@ app.use('/cat', catRouter);
 app.use('/dog', dogRouter);
 app.use('/notes_from_b', notes_from_bRouter);
 
-// uncomment after placing your favicon in /public
+//uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
